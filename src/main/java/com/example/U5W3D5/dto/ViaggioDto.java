68b9eefb5240cfbb1b46a87cb3def0efab2142b8
Vittoria Sacchi <vittoriasacchi7@gmail.com>
2025-06-30 19:16:40 +0200

    Update ViaggioDto.java

--- src/main/java/com/example/U5W3D5/dto/ViaggioDto.java
+++ src/main/java/com/example/U5W3D5/dto/ViaggioDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import java.time.LocalDate;
 
 @Data
-public class ViaggioDto {
+public class ViaggioDTO {
     @NotEmpty(message = "La destinazione dev'essere scelta")
     private String destinazione;
     @NotNull(message = "La data dev'essere obbligatoria")
